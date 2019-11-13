84ea898151481556704e75b070fde7193c383cb0
Julián Idiart <julianidiart1987@hotmail.com>
2019-11-13 01:33:01 +0100

    Generate random numbers button

--- src/components/NumbersContainer/index.js
+++ src/components/NumbersContainer/index.js
@@ -4,23 +4,34 @@ import NumberItem from "../NumberItem";
 const NumbersContainer = () => {
   const [numbers, setNumbers] = useState([]);
   useEffect(() => {
+    generateRandomNumbers();
+    // eslint-disable-next-line
+  }, []);
+  const generateRandomNumbers = () => {
     const numbersArray = [];
     for (let i = 0; i < 10; i++) {
-      numbersArray.push(getRandomInt(-9, 99));
+      numbersArray.push(getRandomNumber(0, 99));
     }
     setNumbers(numbersArray);
-  }, []);
-  const getRandomInt = (min, max) => {
+  };
+  const getRandomNumber = (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
   return (
-    <div className="numbers-container">
-      {numbers.map((number, i) => (
-        <NumberItem key={i} value={number} />
-      ))}
-    </div>
+    <>
+      <div className="numbers-container">
+        {numbers.map((number, i) => (
+          <NumberItem key={i} value={number} />
+        ))}
+      </div>
+      <div className="numbers-container">
+        <button className="button" onClick={generateRandomNumbers}>
+          Generate random numbers
+        </button>
+      </div>
+    </>
   );
 };
 
